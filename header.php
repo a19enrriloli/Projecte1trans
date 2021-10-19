625fa58387229cc2a17d2efa7eb75703cc48b94b
a19enrriloli <73520677+a19enrriloli@users.noreply.github.com>
2021-10-19 13:54:34 +0200

    HEADER

--- header.php
+++ header.php
@@ -1,5 +1,16 @@
-<div id="header">
-    <a href='Pagina_Principal.php'>
-        <img src="img/logo.jpg" alt="Logo Pedralbes" width="200px">
-    </a>
+<div class="header">
+<div class="pag" style=" width: 30%;">
+    <div>
+        <a href='Pagina_Principal.php'>
+            <img class=logo src="logo.jpg" alt="Logo Pedralbes" width="200px">
+        </a>
+    </div>
+</div>
+<h1> CANTINA<h1>
+<div class="header-right">
+    <a href="Pagina_Principal.php"><b>Home</a>
+    <a href="Administracio.php"> Administracio </b></a> 
+    <a href="Menu.php"><b> Menu </b></a> 
+   
+</div>
 </div>
