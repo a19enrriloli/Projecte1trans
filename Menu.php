23063454f65d17a8ca7669fdfc3944992b942f30
a20valzavvas <a20valzavvas@inspedralbes.cat>
2021-10-20 11:43:10 +0200

    jason

--- Menu.php
+++ Menu.php
@@ -20,11 +20,6 @@
     <form id='form'>
     <h2>Productes mati </h2>
     <?php
-<<<<<<< Updated upstream
-
-=======
-        $id=0;
->>>>>>> Stashed changes
         $data = file_get_contents("admin/Pmati.json");
         $products = json_decode($data, true);
         foreach ($products as $prod) {
@@ -35,7 +30,6 @@
             echo "<input type='text' value='0'>";
             echo "<input type='button' value='-' class='treure'></input><br><br></div>";               
         }
-        $id++;
     ?>
     </form>
 
