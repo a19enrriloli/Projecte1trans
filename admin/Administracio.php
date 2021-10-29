80d7ef0dcd6919b060fe162ebcc307e6737367fc
a20valzavvas <a20valzavvas@inspedralbes.cat>
2021-10-29 13:05:51 +0200

    Update Administracio.php

--- admin/Administracio.php
+++ admin/Administracio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Menu</title>
 </head>
 
@@ -12,21 +12,21 @@
     <?php 
     include("./header.php");
     ?>
-    <div class="pag" style="display: flex;">
+    <div>
        
         <div>
-            <h1>Administracio</h1>
+            <h1 class="admin">Administracio</h1>
         </div>
     </div>
-    <div>
+    <div class="tiquet">
     <?php
-        $texto = file_get_contents("../comanda.txt");
-        $texto = nl2br($texto); 
-        echo $texto;
+            $texto = file_get_contents("../comanda.txt");
+            $texto = nl2br($texto);
+            echo $texto;
     ?>
     </div>
-    <div>
-            <a href="../index.php"><button id="back-button" type="button">&#10094 Enrere</button></a>
+    <div style="text-align: center;">
+            <a href="../index.php"><button id="back-button" type="button" class="button">&#10094 Enrere</button></a>
     </div>
     
 </body>
