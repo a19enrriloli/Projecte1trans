70f9bd3b800bbaf472fa7cc8d176ca786315e151
a20valzavvas <a20valzavvas@inspedralbes.cat>
2021-10-29 10:35:01 +0200

    cambios css y img

---
+++ admin/header.php
@@ -0,0 +1,15 @@
+<div class="header">
+<div class="pag" style=" width: 30%;">
+    <div>
+        <a href='index.php'>
+            <img class=logo src="img/logo.jpg" alt="Logo Pedralbes" width="200px">
+        </a>
+    </div>
+</div>
+<h1> CANTINA<h1>
+<div class="header-right">
+    <a href="index.php"><b>Home</a>
+    <a href="Menu.php"><b> Menu </b></a> 
+   <a href="admin/Administracio.php"> Administracio </b></a> 
+</div>
+</div>
